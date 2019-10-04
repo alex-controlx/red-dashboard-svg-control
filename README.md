fca9bffb09dc9357b9ae77a0e66709394c36027e
alex <alex@optimcontrols.com.au>
2019-10-04 18:21:43 +1000

    Updated to version 0.0.3

--- README.md
+++ README.md
@@ -14,16 +14,16 @@ To use controllable SVG components in Template of Node-RED Dashboard follow the 
 Create SVG image in third party software. I used [Boxy SVG](https://boxy-svg.com/) (not sure if there is a 
 free version). If using Boxy SVG click Elements tab at the bottom or open the created SVG file with a text 
 editor (eg. [Visual Studio Code](https://code.visualstudio.com/download)). Find element you want to 
-animate and add two attributes: `id` and `cx-type`. See an example below where attributes
-are `id="pump_P101A"` and `cx-type="cx_color"`
+animate and add `id` attribute. See an example below where `id` attribute is `id="pump_P101A@cx_color"`
 
-`<path id="pump_P101A" cx-type="cx_color" d="M 1018 600 C 1018 609.941 Z" style="fill: rgb(216, 216, 216);">`
+`<path id="pump_P101A@cx_color" d="M 1018 600 C 1018 609.941 Z" style="fill: rgb(216, 216, 216);">`
 
-The `id` attribute is an unique identifier. `cx-type` is a set of types which describes element's
+There are two components of the `id` attribute. The first one is an unique identifier and
+the second one is a set of types which describes element's
 animation. Separate types with a comma `,` to add different types of animation. For example
-`cx-type="cx_color,cx_hide"`
+`id="pump_P101A@cx_color,cx_hide"`
 
-#### `cx-type` applicability (not all tags tested yet)
+#### Where to use the types (not all tags tested yet)
 - `cx_move` is for groups `<g>`, lines `<line>` and text tags `<text>`. It doesn't work with 
 paths `<path>`. Wrap paths into a group for `cx_move`.
 - `cx_color` is for paths `<path>`, shapes `<rect> <circle> <ellipse> <polygon>` and text tags `<text>`.
@@ -40,11 +40,12 @@ the Template node will animate your SVG content.
 
 ### API
 Type of Node-RED message payload with the following topics:
-- `cx_move`+`@unique_id` payload is an object;
-- `cx_color`+`@unique_id` - payload is a string;
-- `cx_status`+`@unique_id` - payload is 12 characters string;
-- `cx_hide`+`@unique_id` - payload is a boolean (true or false).
+- `unique_id@`+`cx_move` payload is an object;
+- `unique_id@`+`cx_color` - payload is a string;
+- `unique_id@`+`cx_status` - payload is 12 characters string;
+- `unique_id@`+`cx_hide` - payload is a boolean (true or false).
 
 ### TODO
+- Test and document usage of `payload` as an array of payloads;
 - Add an example of usage in UI Template (Node-RED).
 
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -28,14 +28,14 @@ angular.module('myApp.view1', ['ngRoute'])
 
   $scope.changeColor = function() {
     if (!$scope.selectedColorEl) return;
-    $scope.msg = { payload: 'rgb(' + Object.values($scope.color).join(',') + ')', topic: cxColor + "@" + $scope.selectedColorEl };
+    $scope.msg = { payload: 'rgb(' + Object.values($scope.color).join(',') + ')', topic: $scope.selectedColorEl };
   };
 
   $scope.selectedOnOffEl = null;
   $scope.switchOn = function(_value) {
     if (!$scope.selectedOnOffEl) return;
     const color = (_value) ? '#eaeaea' : '#8b8b8b';
-    $scope.msg = { payload: color, topic: cxColor + "@" + $scope.selectedOnOffEl };
+    $scope.msg = { payload: color, topic: $scope.selectedOnOffEl };
   };
 
   $scope.position = {
@@ -51,7 +51,7 @@ angular.module('myApp.view1', ['ngRoute'])
       y: $scope.position.y,
       deg: $scope.position.deg
     };
-    $scope.msg = { payload: config, topic: cxMove + "@" + $scope.selectedMoveEl };
+    $scope.msg = { payload: config, topic: scope.selectedMoveEl.split("@")[0] + "@" + cxMove};
   };
   $scope.zeroPosition = function(key) {
     $scope.position[key] = 0;
@@ -62,81 +62,28 @@ angular.module('myApp.view1', ['ngRoute'])
   $scope.newStatus = 'Running';
   $scope.changeStatus = function() {
     if (!$scope.selectedStatusEl) return;
-    $scope.msg = { payload: $scope.newStatus, topic: cxStatus + "@" + $scope.selectedStatusEl };
+    $scope.msg = { payload: $scope.newStatus, topic: $scope.selectedStatusEl };
   };
 
   $scope.selectedHideEl = null;
   $scope.hide = function(toHide) {
     if (!$scope.selectedHideEl) return;
-    $scope.msg = { payload: toHide, topic: cxHide + "@" + $scope.selectedHideEl };
+    $scope.msg = { payload: toHide, topic: scope.selectedHideEl.split("@")[0] + "@" + cxHide };
+  };
+
+  $scope.combinedEl = null;
+  let combinedElRequest = false;
+  $scope.mixedAnimation = function() {
+    if (!$scope.combinedEl) return;
+    combinedElRequest = !combinedElRequest;
+    const color = (combinedElRequest) ? '#eaeaea' : '#8b8b8b';
+    const text = (combinedElRequest) ? 'Running' : 'Stopped';
+    $scope.msg = {
+      payload: [
+        {payload: color, topic: $scope.combinedEl.split("@")[0] + "@" + cxColor},
+        {payload: text, topic: $scope.combinedEl.split("@")[0] + "@" + cxStatus}
+      ]
+    }
   };
 
 }]);
-
-// old way
-function getMatrix(config, box) {
-  // x: 51.95399475097656, y: 54.69599914550781, width: 94.41000366210938, height: 94.41000366210938
-
-  if (!config || !box) return;
-
-  const tx = config.x || 0;
-  const ty = config.y || 0;
-  const deg = config.deg || 0;
-
-    const centre = {
-    x: box.x + box.width/2 || 0,
-    y: box.y + 0 || 0
-  };
-
-  const translation = {
-    x: tx,
-    y: ty
-  };
-  const scaling = {
-    x: 1, // 1.5,
-    y: 1 // 1.5
-
-  };
-  const angleInRadians = deg * (Math.PI / 180);
-  const translationMatrix = [
-    [1, 0, translation.x],
-    [0, 1, translation.y],
-    [0, 0, 1]
-  ];
-  const scalingMatrix = [
-    [scaling.x, 0, 0],
-    [0, scaling.y, 0],
-    [0, 0, 1]
-  ];
-  const rotationMatrix = [
-    [ Math.cos(angleInRadians), -Math.sin(angleInRadians), (-Math.cos(angleInRadians) * centre.x + Math.sin(angleInRadians) * centre.y + centre.x) ],
-    [ Math.sin(angleInRadians), Math.cos(angleInRadians), (-Math.sin(angleInRadians) * centre.x - Math.cos(angleInRadians) * centre.y + centre.y) ],
-    [ 0, 0, 1 ]
-  ];
-
-  const transformMatrix = multiply(multiply(translationMatrix, scalingMatrix), rotationMatrix);
-
-  // matrix(0.594088, -0.516432, 0.62601, 0.720144, -223.586029, 145.949615)
-  return `matrix(` +
-      `${transformMatrix[0][0]}, ` +
-      `${transformMatrix[1][0]}, ` +
-      `${transformMatrix[0][1]}, ` +
-      `${transformMatrix[1][1]}, ` +
-      `${transformMatrix[0][2]}, ` +
-      `${transformMatrix[1][2]}` +
-      `)`;
-
-  function multiply(a, b) {
-    const aNumRows = a.length, aNumCols = a[0].length, bNumCols = b[0].length, m = new Array(aNumRows);  // initialize array of rows
-    for (let r = 0; r < aNumRows; ++r) {
-      m[r] = new Array(bNumCols); // initialize the current row
-      for (let c = 0; c < bNumCols; ++c) {
-        m[r][c] = 0;             // initialize the current cell
-        for (let i = 0; i < aNumCols; ++i) {
-          m[r][c] += a[r][i] * b[i][c];
-        }
-      }
-    }
-    return m;
-  }
-}
